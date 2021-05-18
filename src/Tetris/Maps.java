ad573288aa97c308bd3b91447c9eb25005e645a0
Jessica Stojkovic <jrstojkovic123@gmail.com>
2021-05-18 16:35:01 -0700

    Added Forth rotation for piece1 in Maps class.

--- src/Tetris/Maps.java
+++ src/Tetris/Maps.java
@@ -642,16 +642,16 @@ public class Maps {
                 } else if (k == 3) {
                     for (int i = 0; i < 4; i++) {
                         for (int j = 0; j < 4; j++) {
-                            if (i == 1) {
+                            if (i == 0) {
+                                if (j == 2 || j == 3) {
+                                    piece1[k][i][j].setColored(true);
+                                }
+                            } else if (i == 1) {
                                 if (j == 2) {
                                     piece1[k][i][j].setColored(true);
                                 }
                             } else if (i == 2) {
-                                if (j == 1 || j == 3) {
-                                    piece1[k][i][j].setColored(true);
-                                }
-                            } else if (i == 3) {
-                                if (j == 0) {
+                                if (j == 2) {
                                     piece1[k][i][j].setColored(true);
                                 }
                             }
