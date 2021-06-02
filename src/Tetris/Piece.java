13619f0be7a39abed1c19a7796ba26893a077ee7
Jessica Stojkovic <jrstojkovic123@gmail.com>
2021-06-02 15:22:39 -0700

    Rotation 0 of classic piece 4 can now move down the board

--- src/Tetris/Piece.java
+++ src/Tetris/Piece.java
@@ -498,6 +498,99 @@ public class Piece {
                     }
                 }
             }
+            else if (id == 4){
+                if (r == 0){
+                    for (int i =1; i < 4; i++){
+                        int j = 1;
+                        s = this.map[i][j];
+                        int yValue = s.getY()+1;
+                        if(s.getY() == 29){
+                            return false;
+                        }
+                        else if(gameBoard[s.getX()][yValue].getColored()){
+                            return false;
+                        }
+                    }
+                }
+                else if(r == 1){
+                    for(int i = 1; i <3; i++){
+                        if (i == 1){
+                            int j =1;
+                            s = this.map[i][j];
+                            int yValue = s.getY()+1;
+                            if(s.getY() == 29){
+                                return false;
+                            }
+                            else if(gameBoard[s.getX()][yValue].getColored()){
+                                return false;
+                            }
+                        }
+                        else{
+                            int j = 2;
+                            s = this.map[i][j];
+                            int yValue = s.getY()+1;
+                            if(s.getY() == 29){
+                                return false;
+                            }
+                            else if(gameBoard[s.getX()][yValue].getColored()){
+                                return false;
+                            }
+                        }
+                    }
+                }
+                else if (r == 2){
+                    for(int i = 1; i < 4; i++){
+                        if(i ==1 || i == 3){
+                            int j = 1;
+                            s = this.map[i][j];
+                            int yValue = s.getY()+1;
+                            if(s.getY() == 29){
+                                return false;
+                            }
+                            else if(gameBoard[s.getX()][yValue].getColored()){
+                                return false;
+                            }
+                        }
+                        else{
+                            int j = 2;
+                            s = this.map[i][j];
+                            int yValue = s.getY()+1;
+                            if(s.getY() == 29){
+                                return false;
+                            }
+                            else if(gameBoard[s.getX()][yValue].getColored()){
+                                return false;
+                            }
+                        }
+                    }
+                }
+                else if (r == 3){
+                    for (int i =2; i<4; i++){
+                        if(i == 2){
+                            int j = 2;
+                            s = this.map[i][j];
+                            int yValue = s.getY()+1;
+                            if(s.getY() == 29){
+                                return false;
+                            }
+                            else if(gameBoard[s.getX()][yValue].getColored()){
+                                return false;
+                            }
+                        }
+                        else{
+                            int j =1;
+                            s = this.map[i][j];
+                            int yValue = s.getY()+1;
+                            if(s.getY() == 29){
+                                return false;
+                            }
+                            else if(gameBoard[s.getX()][yValue].getColored()){
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
         }
         return true;
 
