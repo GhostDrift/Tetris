974fa1923ca29cfcd260df424adc0969bdafc24b
Jessica Stojkovic <jrstojkovic123@gmail.com>
2021-08-02 12:45:05 -0700

    Rotation 0 of piece 5 in game 1 can now move right.

--- src/Tetris/Piece.java
+++ src/Tetris/Piece.java
@@ -1778,6 +1778,40 @@ public class Piece {
                         }
                     }
                 }
+            } else if (id == 5){
+                if (r == 0){
+                    for(int i = 2; i<4; i++){
+                        if ( i == 2){
+                            int j = 0;
+                            s = this.map[i][j];
+                            if (s.getColored()) {
+                                int xValue = s.getX() + 1;
+                                //System.out.println("x chordiante: " + s.getX() + " y cordinate: " + s.getY());
+                                if (s.getX() >= 9) {
+                                    //System.out.println("The block is at the right edge of the board");
+                                    return false;
+                                } else if (gameBoard[xValue][s.getY()].getColored()) {
+                                    //System.out.println("the block is on the left of another block");
+                                    return false;
+                                }
+                            }
+                        } else {
+                            int j = 1;
+                            s = this.map[i][j];
+                            if (s.getColored()) {
+                                int xValue = s.getX() + 1;
+                                //System.out.println("x chordiante: " + s.getX() + " y cordinate: " + s.getY());
+                                if (s.getX() >= 9) {
+                                    //System.out.println("The block is at the right edge of the board");
+                                    return false;
+                                } else if (gameBoard[xValue][s.getY()].getColored()) {
+                                    //System.out.println("the block is on the left of another block");
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
         return true;
